07adb07c4f2765185ea88bbd98725cda3dad636f
Noel De Martin <noeldemartin@gmail.com>
2020-12-20 13:34:29 +0100

    Fix lint errors

--- test/integration/ContainerCreation.test.ts
+++ test/integration/ContainerCreation.test.ts
@@ -9,48 +9,47 @@ const port = 6003;
 const baseUrl = `http://localhost:${port}/`;
 
 describe('A server', (): void => {
-    let server: Server;
+  let server: Server;
 
-    beforeAll(async (): Promise<void> => {
-        const factory = await instantiateFromConfig(
-            'urn:solid-server:default:ServerFactory', 'auth-allow-all.json', {
-            'urn:solid-server:default:variable:port': port,
-            'urn:solid-server:default:variable:baseUrl': baseUrl,
-            'urn:solid-server:default:variable:podTemplateFolder': join(__dirname, '../assets/templates'),
-        },
-        ) as HttpServerFactory;
-        server = factory.startServer(port);
+  beforeAll(async(): Promise<void> => {
+    const factory = await instantiateFromConfig(
+      'urn:solid-server:default:ServerFactory', 'auth-allow-all.json', {
+        'urn:solid-server:default:variable:port': port,
+        'urn:solid-server:default:variable:baseUrl': baseUrl,
+        'urn:solid-server:default:variable:podTemplateFolder': join(__dirname, '../assets/templates'),
+      },
+    ) as HttpServerFactory;
+    server = factory.startServer(port);
+  });
+
+  afterAll(async(): Promise<void> => {
+    await new Promise((resolve, reject): void => {
+      server.close((error): void => error ? reject(error) : resolve());
     });
+  });
 
-    afterAll(async (): Promise<void> => {
-        await new Promise((resolve, reject): void => {
-            server.close((error): void => error ? reject(error) : resolve());
-        });
+  it('creates a container.', async(): Promise<void> => {
+    const slug = 'my-container';
+    let response = await fetch(baseUrl, {
+      method: 'POST',
+      headers: {
+        'content-type': 'text/turtle',
+        link: '<http://www.w3.org/ns/ldp#BasicContainer>; rel="type"',
+        slug,
+      },
+      body: '<> <http://www.w3.org/2000/01/rdf-schema#label> "My Container" .',
     });
+    expect(response.status).toBe(201);
+    expect(response.headers.get('location')).toBe(`${baseUrl}${slug}/`);
 
-    it('creates a container', async () => {
-        const slug = 'my-container';
-        let response = await fetch(baseUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'text/turtle',
-                'Link': '<http://www.w3.org/ns/ldp#BasicContainer>; rel="type"',
-                'Slug': slug,
-            },
-            body: '<> <http://www.w3.org/2000/01/rdf-schema#label> "My Container" .',
-        });
-        expect(response.status).toBe(201);
-        expect(response.headers.get('location')).toBe(`${baseUrl}${slug}/`);
-
-        response = await fetch(`${baseUrl}${slug}/`, {
-            headers: {
-                'accept': 'text/turtle'
-            }
-        });
-        expect(response.status).toBe(200);
-
-        const body = await readableToString(response.body as any);
-        expect(body).toContain(`<${baseUrl}${slug}/> <http://www.w3.org/2000/01/rdf-schema#label> "My Container".`);
+    response = await fetch(`${baseUrl}${slug}/`, {
+      headers: {
+        accept: 'text/turtle',
+      },
     });
+    expect(response.status).toBe(200);
 
+    const body = await readableToString(response.body as any);
+    expect(body).toContain(`<${baseUrl}${slug}/> <http://www.w3.org/2000/01/rdf-schema#label> "My Container".`);
+  });
 });
